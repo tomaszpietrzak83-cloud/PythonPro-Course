43d15f8572e7784b8d507130763bdaa682f7c1ba
Tomasz Pietrzak <tomaszpietrzak83@wp.pl>
2026-04-10 19:14:53 +0200

    refactor: improve code readability

--- lesson01/task10_access2.py
+++ lesson01/task10_access2.py
@@ -1,6 +1,6 @@
 hight = int(input("What is your hight? "))
-withAdult = input("Are you with an adult? (yes/no) ")
-if (withAdult.lower() == "yes" and hight >= 120) or (withAdult.lower() == "no" and hight >= 160):
+isWithAdult = input("Are you with an adult? (yes/no) ").lower() == "yes"
+if (isWithAdult and 160 > hight >= 120) or (hight >= 160):
   print("True: 'You can go to the ride!'")
 else:    
   print("False: 'Sorry, you cannot go to the ride.'")
