3d226d54251a07c655b313638089ad3b32f29feb
Tomasz Pietrzak <tomaszpietrzak83@wp.pl>
2026-04-11 09:50:18 +0200

    refactor: code change

--- lesson02/05.py
+++ lesson02/05.py
@@ -1,28 +1,32 @@
 print("Chose a character from list: Dwarf(1), Elf(2), Human(3), Orc(4), custom(5)")
 
 userChoiceOfCharacter = str(input("Your character choice: "))
-if userChoiceOfCharacter == "1":
-    character = "Dwarf"
-elif userChoiceOfCharacter == "2":
-    character = "Elf"
-elif userChoiceOfCharacter == "3":
-    character = "Human"
-elif userChoiceOfCharacter == "4":
-    character = "Orc"
-else:
-    character = input("Your character race: ")
+match userChoiceOfCharacter:
+    case "1":
+        character = "Dwarf"
+    case "2":
+        character = "Elf"
+    case "3":
+        character = "Human"
+    case "4":
+        character = "Orc"
+    case _:
+        character = input("Your character race: ")
 print("You chose: " + character)
 
 print("Chose class: Warrior(1), Mage(2), Archer(3), custom(4)")
+
 userChoiceOfCharacterClass = input("Your class choice: ")
-if userChoiceOfCharacterClass == "1":
-    characterClass = "Warrior"
-elif userChoiceOfCharacterClass == "2":
-    characterClass = "Mage"
-elif userChoiceOfCharacterClass == "3":
-    characterClass = "Archer"
-else:
-    characterClass = str(input("Your character class: "))
+
+match userChoiceOfCharacterClass:
+    case "1":
+        characterClass = "Warrior"
+    case "2":
+        characterClass = "Mage"
+    case "3":
+        characterClass = "Archer"
+    case _:
+        characterClass = str(input("Your character class: "))
 print("You chose: " + characterClass)
 
 print("You created a " + characterClass + " " + character)
