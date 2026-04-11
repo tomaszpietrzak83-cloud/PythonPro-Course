d6828edb2662aa9d20840f6c317c8fc80cf6cc68
Tomasz Pietrzak <tomaszpietrzak83@wp.pl>
2026-04-11 14:11:18 +0200

    style: add lines

--- lesson03/02.py
+++ lesson03/02.py
@@ -1,10 +1,15 @@
 print("We're going to count BMI.")
+
 weight = float(input("What is your weight in kg? "))
 height = float(input("What is your height in cm? "))
+
 bmi = round(weight / ((height / 100) ** 2), 2)
+
 if bmi < 18.5:
     print("Your BMI is", bmi, "which means you are underweight.")
+
 elif 18.5 <= bmi < 25:
     print("Your BMI is", bmi, "which means you are normal weight.")
+    
 else:
     print("Your BMI is", bmi, "which means you are overweight.")
