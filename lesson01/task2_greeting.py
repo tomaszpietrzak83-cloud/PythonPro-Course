96c7a40ae9f93623cb99c8a31d84d48e21cecce1
Tomasz Pietrzak <tomaszpietrzak83@wp.pl>
2026-04-11 09:24:03 +0200

    :refactor var names change

--- lesson01/task2_greeting.py
+++ lesson01/task2_greeting.py
@@ -1,8 +1,8 @@
-import datetime
+import datetime as d
 print("Hello, how are you doing?")
 print("I hope you are doing well.")
 name = input("What is your name?")
 print("Hello, " + name + "!")
-age = int(input("What is your birth year?"))
-currentYear = datetime.datetime.now().year
-print("You are " + str(currentYear - age) + " years old.")
+birthYear = int(input("What is your birth year?"))
+currentYear = d.datetime.now().year
+print("You are " + str(currentYear - birthYear) + " years old.")
