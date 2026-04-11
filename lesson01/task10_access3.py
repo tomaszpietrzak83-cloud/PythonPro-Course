de2935232b4c01af0dc6e6d8de7dab1761737635
Tomasz Pietrzak <tomaszpietrzak83@wp.pl>
2026-04-11 14:23:43 +0200

    refactor: little improvement

--- lesson01/task10_access3.py
+++ lesson01/task10_access3.py
@@ -2,8 +2,9 @@ hight = int(input("What is your hight? "))
 withAdult = input("Are you with an adult? (yes/no) ")
 
 isWithAdult = withAdult.lower() == "yes"
+canRide = (isWithAdult and (160 > hight >= 120)) or  (hight >= 160)
 
-if (isWithAdult and (160 > hight >= 120)) or  (hight >= 160):
-  print((isWithAdult and (160 > hight >= 120)) or  (hight >= 160))
+if canRide == True:
+  print("True")
 else:    
   print("False")
