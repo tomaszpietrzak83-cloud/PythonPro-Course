30536590c37259d94c55eee23ab5e4f1771fc7a1
Tomasz Pietrzak <tomaszpietrzak83@wp.pl>
2026-04-11 09:19:31 +0200

    :refactor var name change

--- lesson01/task5_exchange.py
+++ lesson01/task5_exchange.py
@@ -1,6 +1,6 @@
 print("Hello, welcome to exchange!")
-usdCourseInZloty = 4.0
-print(f"Current exchange rate is {usdCourseInZloty} zloty for 1 dollar.")
+usd_to_pln_rate = 4.0
+print(f"Current exchange rate is {usd_to_pln_rate} zloty for 1 dollar.")
 amountInUsd = float(input("How many dollars do you want to exchange? "))
-amountInZloty = amountInUsd * usdCourseInZloty
+amountInZloty = amountInUsd * usd_to_pln_rate
 print(f"You will get {amountInZloty} zloty for {amountInUsd} dollars.")
