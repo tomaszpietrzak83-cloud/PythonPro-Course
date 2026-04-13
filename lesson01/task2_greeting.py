db255e46ee2e90a42289c60600dbe4e66300b5ae
Tomasz Pietrzak <tomaszpietrzak83@wp.pl>
2026-04-13 22:57:46 +0200

    refactor and style:

--- lesson01/task2_greeting.py
+++ lesson01/task2_greeting.py
@@ -1,8 +1,13 @@
-import datetime as d
+from datetime import datetime
+
 print("Hello, how are you doing?")
 print("I hope you are doing well.")
+
 name = input("What is your name?")
+
 print("Hello, " + name + "!")
+
 birthYear = int(input("What is your birth year?"))
-currentYear = d.datetime.now().year
+currentYear = datetime.now().year
+
 print("You are " + str(currentYear - birthYear) + " years old.")
