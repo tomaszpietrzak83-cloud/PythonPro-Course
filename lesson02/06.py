44d1facb524e86157b79e0f4ed2d996b99132695
Tomasz Pietrzak <tomaszpietrzak83@wp.pl>
2026-04-16 09:57:45 +0200

    fix: lower now is called

--- lesson02/06.py
+++ lesson02/06.py
@@ -1,6 +1,7 @@
 age = int(input("what is your age? "))
 
-drivingLicense = input("do you have a driving license? (yes/no) ").lower
+drivingLicense = input("Do you have a driving license? (yes/no) ").lower()
 
-canDrive = age >= 18 and drivingLicense == "yes"
+
+canDrive = (age >= 18) and (drivingLicense == "yes")
 print(canDrive)
