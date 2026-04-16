0aecb5fa52fb228deab2f7206c8b6dc53b2b03d7
Tomasz Pietrzak <tomaszpietrzak83@wp.pl>
2026-04-16 13:11:31 +0200

    refactor: newer version

--- lesson03/01.py
+++ lesson03/01.py
@@ -4,7 +4,8 @@ grades = [4.5, 3.8, 5.0]
 average = sum(grades) / len(grades)
 status = "student"
 isStudent = status == "student"
-print(type(age))
-print(type(name))
-print(type(average))
-print(type(isStudent))
+
+data = [age, name, average, isStudent]
+
+for position in data:
+    print(position, type(position))
