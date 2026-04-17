b23facab74e320435b7f072695592bff9bd57d34
Tomasz Pietrzak <tomaszpietrzak83@wp.pl>
2026-04-17 23:21:24 +0200

    refactor(07): vars names change, print function upgrade

--- lesson05/07.py
+++ lesson05/07.py
@@ -1,4 +1,4 @@
-exampleSentence = """
+text = """
 Tom walked into the quiet forest at dawn. 
 He carried a small notebook and a pen. 
 The air was cold, and birds were singing. 
@@ -6,19 +6,16 @@ He wrote about everything he saw around him.
 Suddenly something moved between the tall trees. 
 He stopped and listened very carefully to the silence. Night fell."""
 
-sentence = input("Please enter a sentence, or press enter for example sentence: ")
+userText = input("Please enter a sentence, or press enter for example sentence: ")
 
 
-def vowelCutter(text):
+def cutVowels(text):
     newSentence = ""
     for character in text:
         if character in ("aeiouyAEIOUY"):
             continue
         newSentence += character
-    return print(newSentence)
+    return newSentence
 
 
-if sentence == "":
-    vowelCutter(exampleSentence)
-else:
-    vowelCutter(sentence)
+print(cutVowels(text) if userText == "" else cutVowels(userText))
