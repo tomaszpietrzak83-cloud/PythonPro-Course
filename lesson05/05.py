ba9f5aa33ffcb6e21f317d81d095f413058dd23d
Tomasz Pietrzak <tomaszpietrzak83@wp.pl>
2026-04-19 16:20:19 +0200

    refactor: use dynamic array

--- lesson05/05.py
+++ lesson05/05.py
@@ -1,12 +1,7 @@
-results = [
-    [],
-    [],
-    [],
-    [],
-    [],
-]
+results = []
 
 for i in range(1, 6):
+    results.append([])
     for j in range(1, 6):
         results[i - 1].append(i * j)
 
