29107cf6f7a66fbbd5f19718ada2d3f385317d54
Tomasz Pietrzak <tomaszpietrzak83@wp.pl>
2026-04-21 09:35:11 +0200

    refactor: change a little in conditions

--- lesson05/01.py
+++ lesson05/01.py
@@ -5,11 +5,11 @@ except ValueError:
 
 if 0 <= age <= 1:
     print("It appears you are infant.")
-elif 2 <= age <= 12:
+elif age <= 12:
     print("You are child, carry on.")
-elif 13 <= age <= 17:
+elif age <= 17:
     print("You are teenager. Good for you.")
-elif 18 <= age <= 64:
+elif age <= 64:
     print("You'r an adult.")
 else:
     print("You are a senior. Respect!")
