6400c340a0258388267e511c11d9390c06ddb6b8
Tomasz Pietrzak <tomaszpietrzak83@wp.pl>
2026-04-27 11:14:43 +0200

    refactor: change var name

--- lesson07/09.py
+++ lesson07/09.py
@@ -1,15 +1,15 @@
-def repeater(n):
+def repeater(n_times):
 
     def decorator(function):
         print("This function counts executions:")
 
         def status(*args, **kwargs):
 
-            for _ in range(1, n + 1):
+            for _ in range(1, n_times + 1):
                 print()
                 print(f"[{function.__name__}] execution #{_}")
                 function(*args, **kwargs)
-            print("\n", f"There was {n} executions.")
+            print("\n", f"There was {n_times} executions.")
             return
 
         return status
