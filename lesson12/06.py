d35a7fae106b71dd0105a8dadf96ea4fbfd7a0a1
Tomek Codex <tomek.codex.local@example.com>
2026-08-12 13:28:58 +0200

    feat(lesson12): add custom validation exceptions

--- lesson12/06.py
+++ lesson12/06.py
@@ -2,13 +2,40 @@ class InvalidPasswordError(Exception):
     pass
 
 
-def passwordSetter(password):
+class PasswordTooShortError(InvalidPasswordError):
+    def __init__(
+        self,
+        message="Password is too short. It must be at least 8 characters long.",
+    ):
+        super().__init__(message)
+
+
+class PasswordNoUppercaseError(InvalidPasswordError):
+    def __init__(
+        self, message="Password must contain at least one uppercase letter."
+    ):
+        super().__init__(message)
+
+
+def password_setter(password):
     if len(password) < 8:
-        raise InvalidPasswordError("Password must be at least 8 characters long.")
+        raise PasswordTooShortError()
+    if not any(char.isupper() for char in password):
+        raise PasswordNoUppercaseError()
+    return print("Password is valid")
 
 
 try:
-    passwordSetter("short")
+    password_setter("short")
+except (PasswordTooShortError, PasswordNoUppercaseError) as e:
+    print("Error:", e)
 
-except InvalidPasswordError as e:
+try:
+    password_setter("abitlonger")
+except (PasswordTooShortError, PasswordNoUppercaseError) as e:
+    print("Error:", e)
+
+try:
+    password_setter("Withuppercase")
+except (PasswordTooShortError, PasswordNoUppercaseError) as e:
     print("Error:", e)
--- lesson12/09.py
+++ lesson12/09.py
@@ -6,31 +6,43 @@ class BankAccount:
     _balance: float
 
     class LackOfFundsError(Exception):
-        pass
+        def __init__(self, message="Insufficient funds for withdrawal."):
+            super().__init__(message)
+
+    class InvalidAmountError(Exception):
+        def __init__(self, message="Amount must be positive."):
+            super().__init__(message)
 
     @property
     def balance(self):
         return self._balance
 
-    # @balance.setter
-    # def balance(self, value):
-    #     if value < 0:
-    #         raise ValueError("Initial balance cannot be negative.")
-    #     self._balance = value
+    @balance.setter
+    def balance(self, value):
+        if value < 0:
+            raise BankAccount.InvalidAmountError(
+                "Initial balance cannot be negative."
+            )
+
+        self._balance = value
 
     def deposit(self, amount):
+
         if amount <= 0:
-            raise ValueError("Deposit amount must be positive.")
+            raise BankAccount.InvalidAmountError(
+                "Deposit amount must be positive."
+            )
         self._balance += amount
         print(f"Deposited: {amount}. New balance: {self._balance}")
 
     def withdraw(self, amount):
         if amount <= 0:
-            raise ValueError("Withdrawal amount must be positive.")
-        if amount >= self._balance:
-            raise BankAccount.LackOfFundsError(
-                "Insufficient funds for withdrawal."
+            raise BankAccount.InvalidAmountError(
+                "Withdrawal amount must be positive."
             )
+
+        if amount >= self._balance:
+            raise BankAccount.LackOfFundsError()
         self._balance -= amount
         print(f"Withdrew: {amount}. New balance: {self._balance}")
 
@@ -40,7 +52,7 @@ account = BankAccount(_balance=256.0)
 try:
     amount = -250
     account.deposit(amount)
-except ValueError as e:
+except BankAccount.InvalidAmountError as e:
     print(f"Error: {e} incorrect amount: {amount}")
 
 try:
@@ -54,7 +66,7 @@ except BankAccount.LackOfFundsError as e:
 try:
     amount = -50
     account.withdraw(amount)
-except ValueError as e:
+except BankAccount.InvalidAmountError as e:
     print(f"Error: {e} incorrect amount: {amount}")
 
 try:
@@ -62,5 +74,5 @@ try:
     account.deposit(amount1)
     amount2 = 50
     account.withdraw(amount2)
-except (ValueError, BankAccount.LackOfFundsError) as e:
+except (BankAccount.InvalidAmountError, BankAccount.LackOfFundsError) as e:
     print(f"Error: {e}")
